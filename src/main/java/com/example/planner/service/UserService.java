ba6da5fa7eb2c6aae30e5be6e3ff43e11501aa1c
sashabayak <sashabayak@icloud.com>
2026-03-26 12:27:33 +0300

    lab2

--- src/main/java/com/example/planner/service/UserService.java
+++ src/main/java/com/example/planner/service/UserService.java
@@ -15,6 +15,7 @@ import com.example.planner.repository.RoleRepository;
 import com.example.planner.repository.UserRepository;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -23,8 +24,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
