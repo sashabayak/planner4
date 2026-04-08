223cc188514f1abf7e2d8604e8a45098a421dbee
sashabayak <sashabayak@icloud.com>
2026-04-08 21:39:52 +0300

    lab5

--- src/test/java/com/example/planner/service/UserServiceTest.java
+++ src/test/java/com/example/planner/service/UserServiceTest.java
@@ -214,7 +214,27 @@ class UserServiceTest {
 	userService.demonstrateEntityGraphSolution();
 	verify(repository).findAllWithEntityGraph();
   }
+  @Test
+  void createWithRelatedWithTransaction_ShouldUseTransactionalMessage() {
+	when(groupRepository.findById(1L)).thenReturn(Optional.of(group));
+	when(roleRepository.findById(1L)).thenReturn(Optional.of(role));
+	when(repository.save(any())).thenReturn(user);
 
+	userService.createWithRelatedWithTransaction(createDto);
+
+	verify(repository).save(any());
+  }
+
+  @Test
+  void createWithRelatedWithoutTransaction_ShouldUseNonTransactionalMessage() {
+	when(groupRepository.findById(1L)).thenReturn(Optional.of(group));
+	when(roleRepository.findById(1L)).thenReturn(Optional.of(role));
+	when(repository.save(any())).thenReturn(user);
+
+	userService.createWithRelatedWithoutTransaction(createDto);
+
+	verify(repository).save(any());
+  }
   @Test
   void processUserCreation_WithTransaction_ShouldWork() {
 	when(groupRepository.findById(1L)).thenReturn(Optional.of(group));
