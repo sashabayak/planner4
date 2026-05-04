b57a63daace0db737cf8c630aeadb2e0db6fe288
sashabayak <sashabayak@icloud.com>
2026-05-04 20:59:07 +0300

    lab7

--- src/main/java/com/example/planner/config/WebConfig.java
+++ src/main/java/com/example/planner/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-	registry.addMapping("/api/**")
+	registry.addMapping("/**")
 		.allowedOrigins("http://localhost:3000",
 			"https://planner-frontend-njnq.onrender.com")
 		.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
